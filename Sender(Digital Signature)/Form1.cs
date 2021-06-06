2b22b9da82ee0e4ab03fe7aea15e09852bd0df3c
Dainoras Žiukas <65944827+dainoras99@users.noreply.github.com>
2021-06-06 15:34:59 +0300

    Rsa digital signature implementation

--- Sender(Digital Signature)/Form1.cs
+++ Sender(Digital Signature)/Form1.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,6 +20,37 @@ namespace Sender_Digital_Signature_
         }
 
         SimpleTcpClient client;
+        Encoding encoding = Encoding.GetEncoding("437");
+
+        public string HashAndSignBytes()
+        {
+            try
+            {
+                byte[] dataToSign = encoding.GetBytes(messageTextBox.Text);
+
+                RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+
+                RSAParameters Key = RSAalg.ExportParameters(true);
+
+                byte[] signedData = RSAalg.SignData(dataToSign, SHA256.Create());
+
+                string signedString = encoding.GetString(signedData);
+
+                return signedString;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        public string doubleString()
+        {
+            string sendThisString = messageTextBox.Text + "AtSkYrImaSA!@345f" + HashAndSignBytes();
+            return sendThisString;
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -29,13 +61,20 @@ namespace Sender_Digital_Signature_
 
         private void connectButton_Click(object sender, EventArgs e)
         {
-            client.Connect(ipTextBox.Text, Convert.ToInt32(portTextBox.Text));
-            connectButton.Enabled = false;
+            try
+            {
+                client.Connect(ipTextBox.Text, Convert.ToInt32(portTextBox.Text));
+                connectButton.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("Receiver is turned off");
+            }
         }
 
         private void sendButton_Click(object sender, EventArgs e)
         {
-            client.WriteLineAndGetReply(messageTextBox.Text, TimeSpan.FromSeconds(3));
+            client.WriteLineAndGetReply(doubleString(), TimeSpan.FromSeconds(0));
         }
     }
 }
