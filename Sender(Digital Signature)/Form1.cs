1e8a9ae376091d23c5b58550a0f9234ed46bab1f
Dainoras Žiukas <65944827+dainoras99@users.noreply.github.com>
2021-06-06 12:47:41 +0300

    Simple GUI

--- Sender(Digital Signature)/Form1.cs
+++ Sender(Digital Signature)/Form1.cs
@@ -16,5 +16,15 @@ namespace Sender_Digital_Signature_
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
